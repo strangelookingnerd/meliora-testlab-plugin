d5956876b389804efbe2577862feabf346d9b77f
markok <marko.kanala@meliora.fi>
2015-07-10 13:28:26 +0300

    - add support for test case parameter variables - prepare release

---
+++ src/main/java/fi/meliora/testlab/ext/rest/model/KeyValuePair.java
@@ -0,0 +1,37 @@
+package fi.meliora.testlab.ext.rest.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Encapsulates a single key-value pair.
+ *
+ * @author Marko Kanala
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class KeyValuePair extends ModelObject {
+    @XmlElement
+    private String key;
+
+    @XmlElement
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
